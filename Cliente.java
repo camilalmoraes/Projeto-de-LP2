2081cf46e2ce5c443f317075b402fb2006487473
Henrique <henriquebandeira307@gmail.com>
2023-05-25 17:54:58 -0300

    Mudando Código, fazendo um chat para aprendizado

--- Cliente.java
+++ Cliente.java
@@ -1,25 +1,61 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Cliente{
-    public static int vez;
+    
+    private Socket socket;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
+
+    public Cliente(Socket socket){
+        try{
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }catch(IOException e){}
+    }
+
+    public void sendMessage(){
+        try{
+            Scanner sc = new Scanner(System.in);
+            while(socket.isConnected()){
+                String messageToSend = sc.nextLine();
+                bufferedWriter.write(messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        }catch(IOException e){} 
+    }
+
+    public void receiveMessage(){
+        new Thread(new Runnable() {
+            @Override
+            public void run(){
+                String msgFromGroup;
+
+                while(socket.isConnected()){
+                    try{
+                        msgFromGroup = bufferedReader.readLine();
+                        System.out.printf("\n%s",msgFromGroup);
+                    }catch(IOException e){
+
+                    }
+                }
+            }
+        }).start();
+    }
+
     public static void main(String[] args) throws UnknownHostException, IOException {
-        Socket s = new Socket("localhost", 4444);
-        DataInputStream in = new DataInputStream(s.getInputStream());
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
-                
-        out.writeUTF("Entrar na Fila");
-        vez = in.readInt();
-        System.out.printf("Você entrou no jogo, como jogador %d",vez+1);
-     
-        
-        // new Jogo(in,out,vez);
-        
-        in.close();
-        out.close();
-        s.close();
+        Socket socket = new Socket("localhost", 4444);
+        Cliente cliente = new Cliente(socket);
+        cliente.receiveMessage();
+        cliente.sendMessage();
+        socket.close();
     }
 }
--- Server/Servidor.java
+++ Server/Servidor.java
@@ -1,22 +1,84 @@
 package Server;
+import java.util.ArrayList;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Servidor {
-    public static void main(String[] args) throws IOException {
-        Executor exec = Executors.newCachedThreadPool();
-        try {
-            ServerSocket s = new ServerSocket(4444);
-            while (true) {
-                Socket ns = s.accept();
-                Socket ns2 = s.accept();
-                exec.execute(new Controle(ns,ns2));
-            }
-        }catch (IOException e){
+public class Servidor implements Runnable{
+    public static void main(String[] args){
+        Servidor server = new Servidor();
+        server.run();
+    }
+    public ArrayList<Controle> connections = new ArrayList<>();
+    
+    @Override
 
+    public void run() {
+        Executor exec = Executors.newCachedThreadPool();
+        try{
+            ServerSocket serverSocket = new ServerSocket(4444);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Controle controle = new Controle(socket);
+                connections.add(controle);
+                exec.execute(controle);
+            }
+        }catch(IOException e){}
+    }
+    
+    public class Controle implements Runnable {
+        private Socket socket;
+        private BufferedWriter bufferedWriter;
+        private BufferedReader bufferedReader;
+        
+        public Controle(Socket socket) {
+            try{
+                this.socket = socket;
+                this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                
+            }catch(IOException e){}
         }
+       
+        @Override
+        public void run(){
+            String messagemFromClient;
+            while (this.socket.isConnected()){
+                try{
+                    messagemFromClient = bufferedReader.readLine();
+                    sendToAll(messagemFromClient);
+                }catch(IOException e){
+                    
+                }
+            }
+        }
+    
+        public void sendToAll(String messageToSend){
+            for(Controle controle: connections){
+                try{
+                    System.out.printf("\n%s",messageToSend);
+                    controle.bufferedWriter.write(messageToSend);
+                    controle.bufferedWriter.newLine();
+                    controle.bufferedWriter.flush();
+                }catch(IOException e){
+    
+                }
+            }
+        }
+
+        // public Object ByteToObject(byte[] bits) throws IOException, ClassNotFoundException{
+        //     ByteArrayInputStream bis = new ByteArrayInputStream(bits);
+        //     ObjectInput in = null;
+        //     in = new ObjectInputStream(bis);
+        //     Object o = in.readObject(); 
+            
+        //     return o;
+        // }
     }
 }
